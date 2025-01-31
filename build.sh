a70cefd907fac2be18c3a68af80cd69097dda4cb
Juber Badi <juber.badi@teksun.com>
2025-01-31 11:32:28 +0530

    all code 7

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install system dependencies for Chromium
-apt-get update
-apt-get install -y \
-    libxfixes3 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libxrandr2 \
-    libasound2 libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 \
-    libnss3 libxss1 libxshmfence1
-
