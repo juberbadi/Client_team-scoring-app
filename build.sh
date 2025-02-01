5d867ddba458d2b6e818444eb5e6f14c7a188f6a
Juber Badi <50127293+juberbadi@users.noreply.github.com>
2025-02-01 05:49:29 +0000

    add build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-apt-get update 
-apt-get install -y libxfixes3  
-apt-get install -y libxfixes3 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libxrandr2 libasound2 libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libnss3 libxss1 libxshmfence1  
+sudo apt-get update 
+sudo apt-get install -y libxfixes3  
+sudo apt-get install -y libxfixes3 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxi6 libxtst6 libxrandr2 libasound2 libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libnss3 libxss1 libxshmfence1  
 # npx puppeteer browsers install chrome 
 npx puppeteer browsers install chrome --path /opt/render/.cache/puppeteer
